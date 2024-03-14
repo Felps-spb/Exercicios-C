08371884fe2b1dcdcf65a60c9a8d6ba68e197dcb
Luiz Felipe <62891000+Felps-spb@users.noreply.github.com>
2024-03-14 16:39:57 -0300

    Add files via upload

---
+++ estudovetores.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main(){
+   
+   int v[5];
+   
+   float m;
+   
+    v[0]= 50;
+    v[1]= 40;
+    v[2]= 30;
+    v[3]= 20;
+    v[4]= 10;
+    
+    m = (v[0] + v[1] + v[2] + v[3] +v[4]) /5;
+    printf("resutaldo: %f \n", m);
+}
